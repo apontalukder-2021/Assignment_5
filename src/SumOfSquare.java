8ec0ccd7c1d59472d4a4d13960c576a9e0ddd7fb
Apon <cse202104019@shu.edu.bd>
2024-11-21 13:50:03 +0600

    Sum of the square problem solve.

---
+++ src/SumOfSquare.java
@@ -0,0 +1,11 @@
+public class SumOfSquare {
+        public static void main(String[] args) {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++) {
+                sum += i * i;
+            }
+            System.out.println("The sum of all squares between 1 and 100 (inclusive) is: " + sum);
+        }
+    }
+
+
