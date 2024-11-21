7831d15df5db76cc929a9741969e735e11ca5291
Apon <cse202104019@shu.edu.bd>
2024-11-21 13:46:03 +0600

    Sum of the Even Number problem solve.

---
+++ src/SumOfEvenNumber.java
@@ -0,0 +1,10 @@
+public class SumOfEvenNumber {
+        public static void main(String[] args) {
+            int sum = 0;
+            for (int i = 2; i <= 100; i += 2) {
+                sum += i;
+            }
+            System.out.println("The sum of all even numbers between 2 and 100 (inclusive) is: " + sum);
+        }
+    }
+
